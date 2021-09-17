a65a149c2f1301ef30b16d743bb019ea1f04b9e6
Renzo Espinoza <renzo.espinoza01@gmail.com>
2021-09-16 23:33:32 -0500

    Finished project besides recommendation implementation

---
+++ src/components/Budget.js
@@ -0,0 +1,38 @@
+import React, {useState, useContext} from 'react';
+import { GlobalContext} from '../context/GlobalState';
+
+
+
+export const Budget = () => {
+    const [amount, setAmount] = useState(0);
+    const [budgetVal, setBudgetVal] = useState(0);
+
+    const { transactions } = useContext(GlobalContext);
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const remainingFunds = (budgetVal - (-total));
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        setBudgetVal(amount);
+
+        //console.log(budgetVal);
+    }
+
+    return (
+        <div>
+            <h4>
+            Budget
+            </h4>
+            <h1>
+            <em>${budgetVal}.00 (Remaining Funds: ${remainingFunds}.00)</em>
+            </h1>
+            <form onSubmit = {handleSubmit} action = "#">
+                <div classNameName = "form control">
+                    <input type = "number" value = {amount} onChange = {(e) => setAmount(e.target.value)} placeholder = "Enter budget..." />
+                </div>
+                <button type = "submit " className = "btn"> Add Budget </button>
+            </form>
+        </div>
+    )
+}
---
+++ src/components/Recommendations.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Recommendations = () => {
+    return (
+        <div>
+            <h3 className = "center"> Recommended Cuts </h3>
+            <ul>
+                <li>Remove housing</li>
+                <li>Remove chupapaya</li>
+            </ul>
+        </div>
+    )
+}
