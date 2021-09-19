b2b6fc7c61b7c4b8868442439412b4d54bb3c578
Renzo Espinoza <renzo.espinoza01@gmail.com>
2021-09-19 14:56:48 -0500

    Started adding recommendation list functionality

--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Budget} from './Budget';
+import {Transaction} from './Transaction';
 
 export const Recommendations = () => {
+    const { transactions } = useContext(GlobalContext);
+
     return (
         <div>
             <h3 className = "center"> Recommended Cuts </h3>
             <ul>
-                <li>Remove housing</li>
+                <li>Remove {transaction}</li>
                 <li>Remove chupapaya</li>
             </ul>
         </div>
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -9,7 +9,7 @@ export const TransactionList = () => {
         <div>
             <h3> Transaction History </h3>
             <ul className = "list">
-                {transactions.map(transaction => (<Transaction key={transaction.id} transaction = {transaction} />))}
+                {transactions.map(transaction => (<Transaction key={transaction.id} transaction = {transaction}/>))}
 
             </ul>
         </div>
